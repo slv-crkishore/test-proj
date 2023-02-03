232cfbbb9693a554c289609a92d9160e30892e59
kishore <ramkishore.ram3@gmail.com>
2023-02-03 15:46:09 +0530

    code updated and basic site ready

---
+++ coding_ninjas/src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About the App</h1>
+      <p>
+        This is a sample React app with a header and footer, and multiple pages.
+      </p>
+    </div>
+  );
+};
+
+export default About;
